669171b282b51da8d6e9b55fab7cde8f1e11652c
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-07-25 00:57:07 +0430

    nav jquery deleted

--- public/js/instagram.js
+++ public/js/instagram.js
@@ -1,19 +1,5 @@
 $(document).ready ( function () {
 
-  $(".input-container").click (() => {
-    $target = $(".input-text");
-    $(".close-logo").show();
-    $target.width(162);
-  });
-  
-  $(document).click((e) => {
-    $contain = $(".input-text, .input-container, .search-logo");
-    if(!$contain.is(e.target)) {
-      $(".close-logo").hide(1);
-      setTimeout("$target.width(63.63)", 10);
-    }
-  });
-
   $(".contain-pic, .story-text").click( () => {
     $(".container").hide();
     $("section").css({
