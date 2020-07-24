54e30aae22f6de7d171d511c464b0c6d8f7d681b
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-07-25 00:54:07 +0430

    nav changed to react

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,23 @@ import React, { Component } from 'react';
 export default class Nav extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: '', showSpan: false, widthText: ""};
     this.changeValue = this.changeValue.bind(this);
+    this.onClickDiv = this.onClickDiv.bind(this);
+    this.onClickClose = this.onClickClose.bind(this);
   }
 
   changeValue(event) {
     this.setState({value: event.target.value});
+  }
+
+  onClickDiv() {
+    this.setState({showSpan: true, widthText: 162}); 
+  }
+
+  onClickClose(event) {
+    event.stopPropagation(); /*impo*/
+    this.setState({showSpan: false, widthText: 63.63});
   }
 
   render() {
@@ -16,10 +27,12 @@ export default class Nav extends React.Component {
       <nav>
         <div id="logo"><h1 id="logo-style">My Instagram</h1></div>
         <div id="seacrh">
-          <div className="input-container">
+          <div className="input-container" onClick={this.onClickDiv} onBlur={this.onClickClose} >
             <img className="search-logo" src="./images/search-logo.png" />
-            <input className="input-text" type="text" placeholder="Search" />
-            <span className="close-logo">×</span>
+            <input className="input-text" type="text" placeholder="Search" value={this.state.value} 
+              onChange={this.changeValue} style={{width: this.state.widthText ? this.state.widthText : ""}} />
+            <span className="close-logo" onClick={this.onClickClose} onBlur={this.onClickClose}
+            style={{display: this.state.showSpan ? "block" : "none"}} >×</span>
           </div>
         </div>
         <div className="right">
