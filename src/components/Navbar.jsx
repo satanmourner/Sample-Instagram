bd5da17ef1ab62e9aae179c13dd63022d726255c
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-07-26 00:47:44 +0430

    bug fixed in search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,8 @@ export default class Nav extends React.Component {
     this.changeValue = this.changeValue.bind(this);
     this.onClickDiv = this.onClickDiv.bind(this);
     this.onClickClose = this.onClickClose.bind(this);
+    this.createRef = React.createRef(); /*impo*/
+    this.handleClickOutside = this.handleClickOutside.bind(this);
   }
 
   changeValue(event) {
@@ -22,16 +24,30 @@ export default class Nav extends React.Component {
     this.setState({showSpan: false, widthText: 63.63});
   }
 
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+}
+
+  componentWillUnmount() {
+      document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  handleClickOutside(event) {
+    if (this.createRef && !this.createRef.current.contains(event.target)) {
+      this.setState({showSpan: false, widthText: 63.63});
+    }
+  }
+
   render() {
     return (
       <nav>
         <div id="logo"><h1 id="logo-style">My Instagram</h1></div>
         <div id="seacrh">
-          <div className="input-container" onClick={this.onClickDiv} onBlur={this.onClickClose} >
+          <div className="input-container" onClick={this.onClickDiv} ref={this.createRef} >
             <img className="search-logo" src="./images/search-logo.png" />
             <input className="input-text" type="text" placeholder="Search" value={this.state.value} 
               onChange={this.changeValue} style={{width: this.state.widthText ? this.state.widthText : ""}} />
-            <span className="close-logo" onClick={this.onClickClose} onBlur={this.onClickClose}
+            <span className="close-logo" onClick={this.onClickClose}
             style={{display: this.state.showSpan ? "block" : "none"}} >×</span>
           </div>
         </div>
