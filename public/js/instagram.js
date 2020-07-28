a40fe6e24ec585ade58e50bc010b32f0b5c58487
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-07-28 16:32:51 +0430

    more section jquery deleted

--- public/js/instagram.js
+++ public/js/instagram.js
@@ -2,31 +2,11 @@ $(document).ready ( function () {
 
   $("img").attr("draggable", "false");
 
-  $(".more, .borderDown").click( () => {
-    $(".more-section").fadeToggle(80);
-    $(".story").css("filter", "blur(2.5px)");
-  });
-  $(document).click((e) => {
-    $except = $(".more-section, .more");
-    if($(".more.section") && !$except.is(e.target)) {
-      $(".more-section").hide();
-    }
-  })
-
   var array = $(".contain-story");
   for( let i = 1, j= 90; i < array.length; i++, j+=90) {
     array[i].style.transform = "translateX(" + j + "px)";
   }
-
-  // $(".arrow-story").click( () => {
-  //   $(".body-story").css("transform3d", "skewZ(12deg)")
-  // });
-
-
-  // $(".arrow-story").click( () => {
-  //   $(".body-story").css("transform", "rotateY(60deg");
-  // })
-
+  
   $(".message-container").click( function() {
     $(this).css("width", "90%");
     $(".message > i").hide();
