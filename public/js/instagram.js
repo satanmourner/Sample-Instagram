5ebaf7dde0d65a41fb871e9f8656f62267b51521
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-07-28 21:27:04 +0430

    input jquery deleted

--- public/js/instagram.js
+++ public/js/instagram.js
@@ -6,16 +6,7 @@ $(document).ready ( function () {
   for( let i = 1, j= 90; i < array.length; i++, j+=90) {
     array[i].style.transform = "translateX(" + j + "px)";
   }
-  
+
 });
-
-function resizeText(text) {
-  
-  (text.value) ? $(".message-container > input").show() && $(".react-emojies").hide() 
-  : $(".message-container > input").hide() && $(".react-emojies").css({"display": "flex",
-  "flex-direction": "column"});
-}
-
-
 
 
