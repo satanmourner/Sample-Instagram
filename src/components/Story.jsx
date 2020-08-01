d3d744e4c7c538b0a5f3eb72a90cf13e0ed71c00
Sanaz Mahmoudi <mahmoudi.sanaz59@gmail.com>
2020-08-01 17:55:48 +0430

    story's list implemented on react

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -42,94 +42,16 @@ export default class Story extends React.Component {
     return (
       <div className="stories">
         <div className="all-stories" style={{transform: this.state.trans}} >
+        {userStory.map(user => (
           <div className="contain-story">
             <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar.png" />
+              <img className="story-pic" src={user.imgSrc} />
             </div>
             <div className="story-text" onClick={this.show}>
-              <a href="#">id no.1</a>
+              <a href="#">{user.name}</a>
             </div>
           </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar-other.jpeg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.2</a>
-            </div>
-          </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar-other2.jpg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.3</a>
-            </div>
-          </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar-other.jpeg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.4</a>
-            </div>
-          </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar-other2.jpg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.5</a>
-            </div>
-          </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar-other.jpeg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.6</a>
-            </div>
-          </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar-other2.jpg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.7</a>
-            </div>
-          </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/prof.jpg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.8</a>
-            </div>
-          </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar-other2.jpg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.9</a>
-            </div>
-          </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar-other.jpeg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.10</a>
-            </div>
-          </div>
-          <div className="contain-story">
-            <div className="contain-pic" onClick={this.show}>
-              <img className="story-pic" src="./images/avatar-other2.jpg" />
-            </div>
-            <div className="story-text" onClick={this.show}>
-              <a href="#">id no.11</a>
-            </div>
-          </div>
+        ))};
         </div>
         <div className="arrows">
           <div className="arrow-left" style={{display: this.state.showLeft ? "block" : "none"}} onClick={this.transformHide} >
@@ -143,3 +65,17 @@ export default class Story extends React.Component {
     );
   }
 }
+
+const userStory = [
+  {id: 1, name: "user 1", imgSrc: "./images/avatar.png" },
+  {id: 2, name: "user 2", imgSrc: "./images/avatar-other.jpeg"},
+  {id: 3, name: "user 3", imgSrc: "./images/avatar-other2.jpg"},
+  {id: 4, name: "user 4", imgSrc: "./images/avatar-other.jpeg"},
+  {id: 5, name: "user 5", imgSrc: "./images/avatar-other2.jpg"},
+  {id: 6, name: "user 6", imgSrc: "./images/avatar-other.jpeg"},
+  {id: 7, name: "user 7", imgSrc: "./images/avatar-other2.jpg"},
+  {id: 8, name: "user 8", imgSrc: "./images/prof.jpg"},
+  {id: 9, name: "user 9", imgSrc: "./images/avatar-other2.jpg"},
+  {id: 10, name: "user 10", imgSrc: "./images/avatar-other.jpeg"},
+  {id: 11, name: "user 11", imgSrc: "./images/avatar-other2.jpg"},
+]
